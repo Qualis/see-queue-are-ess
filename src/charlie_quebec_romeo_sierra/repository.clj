b1e063f7ec380d368bb3af3c440fdc7fb07f4213
Sean Van Osselaer <svanosselaer@aconex.com>
2017-08-17 09:01:03 +1000

    Starting to work on repository layer

--- src/charlie_quebec_romeo_sierra/repository.clj
+++ src/charlie_quebec_romeo_sierra/repository.clj
@@ -1,4 +1,38 @@
-(ns charlie-quebec-romeo-sierra.repository)
+(ns charlie-quebec-romeo-sierra.repository
+  (:require [kinsky.client :as client]
+            [kinsky.async :as async]
+            [clj-uuid :as uuid]
+            [clojure.core.async :refer [go go-loop <! >!! put!]]))
+
+(def consumers (atom {}))
+
+(defn- create-producer
+  []
+  (client/producer {:bootstrap.servers "localhost:9092"}
+                   :keyword
+                   :edn))
+
+(def producer (memoize create-producer))
+
+(defn- register-consumer
+  [type_of consumer]
+  (swap! consumers assoc type_of consumer))
+
+(defn create-consumer
+  [type_of]
+  (register-consumer type_of
+                     (async/consumer
+                       {:bootstrap.servers "localhost:9092"
+                        :group.id type_of}
+                       (client/keyword-deserializer)
+                       (client/edn-deserializer))))
+
+(def consumer (memoize create-consumer))
 
 (defn produce
-  [events])
+  [events]
+  (doseq [event events]
+    (client/send! (producer)
+                  (.type-of event)
+                  (.aggregate-identifier event)
+                  (.data event))))
--- test/charlie_quebec_romeo_sierra/repository_test.clj
+++ test/charlie_quebec_romeo_sierra/repository_test.clj
@@ -1,4 +1,58 @@
 (ns charlie-quebec-romeo-sierra.repository-test
-  (:require [charlie-quebec-romeo-sierra.repository
-             :as repository
-             :refer :all]))
+  (:require [charlie-quebec-romeo-sierra.repository :as repository]
+            [charlie-quebec-romeo-sierra.event
+             :as event :refer :all]
+            [kinsky.client :as client]
+            [kinsky.async :as async]
+            [clj-uuid :as uuid]
+            [clojure.core.async :refer [promise-chan promise-chan]]
+            [midje.open-protocols :refer [defrecord-openly]])
+  (:use [midje.sweet :only [facts fact => provided irrelevant]]))
+
+(facts
+
+  (require '[charlie-quebec-romeo-sierra.repository :as repository
+             :refer :all]
+           :reload)
+
+  (defrecord-openly TestEvent []
+    event/Event
+    (data [this] ..data..)
+    (aggregate-identifier [this] ..identifier..)
+    (type-of [this] ..type..))
+
+  (fact
+    "should create producer"
+    (#'repository/create-producer) => ..producer..
+    (provided
+      (client/producer {:bootstrap.servers "localhost:9092"}
+                       :keyword
+                       :edn) => ..producer..))
+
+  (fact
+    "should register consumer"
+    (#'repository/register-consumer ..type_of.. ..consumer..)
+    @repository/consumers => {..type_of.. ..consumer..})
+
+  (fact
+    "should create consumer"
+    (#'repository/create-consumer ..type_of..) => irrelevant
+    (provided
+      (client/keyword-deserializer) => ..keyword_deserializer..
+      (client/edn-deserializer) => ..value_deserializer..
+      (async/consumer {:bootstrap.servers "localhost:9092"
+                       :group.id ..type_of..}
+                      ..keyword_deserializer..
+                      ..value_deserializer..) => ..consumer..
+      (#'repository/register-consumer ..type_of.. ..consumer..) => irrelevant))
+
+  (fact
+    "should produce events"
+    (let [event (->TestEvent)]
+      (repository/produce [event]) => irrelevant
+      (provided
+        (#'repository/producer) => ..producer..
+        (client/send! ..producer..
+                      ..type..
+                      ..identifier..
+                      ..data..) => ..result..))))
